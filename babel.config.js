9c790c4c192a7af3e7b611f00402acb97dc0c232
Ralph Schuler <48420064+ralphschuler@users.noreply.github.com>
2024-06-22 07:26:11 +0000

    More fixes

--- babel.config.js
+++ babel.config.js
@@ -6,7 +6,21 @@ module.exports = function (api) {
         "babel-preset-expo", 
         { jsxImportSource: "nativewind" }
       ],
-      "nativewind/babel"
+      "nativewind/babel".
+      ['module-resolver', {
+        alias: {
+          'crypto': 'react-native-quick-crypto',
+          'node:crypto': 'react-native-quick-crypto',
+          'stream': 'stream-browserify',
+          'node:stream': 'stream-browserify',
+          'net': 'react-native-tcp-socket',
+          'node:net': 'react-native-tcp-socket',
+          'os': 'os-browserify',
+          'node:os': 'os-browserify',
+          'path': 'path-browserify',
+          'node:path': 'path-browserify'
+        }
+      }],
     ],
     plugins: [
       [
