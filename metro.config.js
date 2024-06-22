9de87e4b04c388b4690c1a83c8749d1055ba887c
Ralph Schuler <48420064+ralphschuler@users.noreply.github.com>
2024-06-22 10:55:16 +0000

    Fix

--- metro.config.js
+++ metro.config.js
@@ -4,5 +4,10 @@ const { withNativeWind } = require('nativewind/metro');
 const config = getDefaultConfig(__dirname);
 
 config.resolver.unstable_enablePackageExports = true;
+config.resolver.unstable_conditionNames = [
+  'browser',
+  'require',
+  'react-native',
+];
 
-module.exports = withNativeWind(config, { input: './global.css' })
+module.exports = withNativeWind(config, { input: './global.css' });
