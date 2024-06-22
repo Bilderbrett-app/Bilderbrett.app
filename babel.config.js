46d4b575dbcb2b9b25ee4e42da02b888c1b1e901
Ralph Schuler <48420064+ralphschuler@users.noreply.github.com>
2024-06-22 11:55:57 +0000

    Fix

--- babel.config.js
+++ babel.config.js
@@ -30,7 +30,7 @@ module.exports = function (api) {
           'libp2p': 'libp2p/dist/index.min.js',
           '@libp2p/websockets': '@libp2p/websockets/dist/index.min.js',
           '@libp2p/bootstrap': '@libp2p/bootstrap/dist/index.min.js',
-          '#@chainsafe/libp2p-noise': '@chainsafe/libp2p-noise/dist/index.min.js'
+          '@chainsafe/libp2p-noise': '@chainsafe/libp2p-noise/dist/index.min.js'
         }
       }],
       "react-native-reanimated/plugin"
