b292ed30c437b394a6ac49d5d003b45c934754a4
Ralph Schuler <ralph.schuler@nyphon.de>
2024-06-23 00:05:42 +0200

    Update metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -7,7 +7,12 @@ const config = getDefaultConfig(__dirname);
 
 config.resetCache = true;
 config.resolver.unstable_enablePackageExports = true;
-config.resolver.unstable_enableSymlinks = true
+config.resolver.unstable_enableSymlinks = true;
+config.resolver.unstable_conditionNames = [
+  "browser",
+  "require",
+  "react-native"
+]
 
 config.cacheStores = ({ FileStore }) => [
   new FileStore({
