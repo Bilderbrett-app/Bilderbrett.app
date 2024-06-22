e4e012871d8b005237d0ed5b50b34c1c3aea33eb
Ralph Schuler <48420064+ralphschuler@users.noreply.github.com>
2024-06-22 10:39:46 +0000

    Fix

--- metro.config.js
+++ metro.config.js
@@ -1,12 +1,8 @@
 const { getDefaultConfig, mergeConfig } = require("expo/metro-config");
 const { withNativeWind } = require('nativewind/metro');
 
-const defaultConfig = getDefaultConfig(__dirname);
+const config = getDefaultConfig(__dirname);
 
-const config = {
-  resolver: {
-    unstable_enablePackageExports: true,
-  }
-};
+config.resolver.unstable_enablePackageExports: true;
 
-module.exports = withNativeWind(mergeConfig(defaultConfig, config), { input: './global.css' })
+module.exports = withNativeWind(config, { input: './global.css' })
