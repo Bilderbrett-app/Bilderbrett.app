fc58e8593f60c5c7835dc3474b28b9c4a2212ed6
Ralph Schuler <48420064+ralphschuler@users.noreply.github.com>
2024-06-22 12:54:21 +0000

    Fix

--- babel.config.js
+++ babel.config.js
@@ -28,7 +28,6 @@ module.exports = function (api) {
           'process': 'react-native-process-shim',
           'node:process': 'react-native-process-shim',
 
-          /*
           'libp2p': 'libp2p/dist/src',
           '@libp2p/websockets': '@libp2p/websockets/dist/src',
           '@libp2p/bootstrap': '@libp2p/bootstrap/dist/src',
@@ -48,8 +47,6 @@ module.exports = function (api) {
           '@chainsafe/libp2p-noise': '@chainsafe/libp2p-noise/dist/src',
           '@chainsafe/libp2p-yamux': '@chainsafe/libp2p-yamux/dist/src',
           '@chainsafe/libp2p-gossipsub': '@chainsafe/libp2p-gossipsub/dist/src',
-
-          */
         }
       }],
       "react-native-reanimated/plugin"
